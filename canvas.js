e1cff85c2028dbfc88b3b861dabe06acb92085fa
Yanjia <liyanjia92@gmail.com>
2014-12-03 20:44:13 -0500

    Little modification
    Start to understand the source code

--- canvas.js
+++ canvas.js
@@ -1,14 +1,11 @@
 //Canvas Animation
 var canvas;
-var c;
+var c;// An object represent the current paintbrush
 var ONE_FRAME_TIME = 1000 / 60 ;
 var x=10+30*4,y=10; //starting position of a shape
 var gameOver=false;
 
 
-var drawLoop = function(){
-	drawGame();
-}
 var animFrame = window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame    ||
@@ -17,7 +14,7 @@ var animFrame = window.requestAnimationFrame ||
         null ;
 
 var recursiveAnim = function() {
-        drawLoop();
+        drawGame();
         animFrame( recursiveAnim );
 };
 
@@ -29,22 +26,28 @@ window.onload = function(){
 	drawBackground();
 }
 
-//**********user clicks and then go,this is where the game begins**********
-function init(){         
-    animFrame( recursiveAnim );
-    setInterval(blockUpdate,10);
+//ENTRANCE OF THE PROGRAM
+//**********User clicks and then go,this is where the game begins**********
+function init(){ 
+    // Call recursiveAnim function recursively to implement a forever loop
+    // This function is responsible for canvas drawing. It is non-block.    
+    animFrame( recursiveAnim ); 
+    // Call blockUpdate every 10ms.
+    // This function is responsible for calculating the status of each block. It is also non-block.
+    setInterval(blockUpdate,10); 
 }
 
 
 //***********To draw on the canvas per frame *********************
 function drawGame(){
 	c.clearRect(0,0,canvas.width,canvas.height);
+	// Draw the background canvas of the game
 	drawBackground();	
+	// Draw each block
 	drawStackBlocks();
-
+	// The moving shape on screen. Just a test.
 	drawLshape_left(c,x,y);
-
-	//draw words
+	// Draw text for game information
 	var gradient=c.createLinearGradient(0,0,canvas.width,0);
 	gradient.addColorStop("0","magenta");
 	gradient.addColorStop("0.5","blue");
@@ -52,14 +55,6 @@ function drawGame(){
 	c.font = "20px  Verdana";
 	c.fillStyle=gradient;
 	c.fillText("Rows removed: "+rows_completed, 350, 50);
-
-	//drawIshape(c,120,120);
-	//drawSquare(c,240,240);
-	//drawLshape_right(c,240,360);
-	//drawTRIshape(c,240,500);
-	//drawLIshape_right(c, 100,500);
-	//drawLIshape_left(c, 100,300);
-	y+=1;
 }
 
 //draw the background stuff
@@ -88,91 +83,7 @@ function drawLshape_left(c,x,y){
 	c.strokeRect(x,y+30,30,30);
 	c.strokeRect(x,y+60,30,30);
 	c.strokeRect(x-30,y+60,30,30);
-}
-
-function drawIshape(c,x,y){
-	c.fillStyle ="yellow";
-	c.strokeStyle = 'black';
-	c.lineWidth=1;
-	c.fillRect(x,y,30,30);
-	c.fillRect(x+30,y,30,30);
-	c.fillRect(x+60,y,30,30);
-	c.fillRect(x+90,y,30,30);	
-	c.strokeRect(x,y,30,30);	
-	c.strokeRect(x+30,y,30,30);
-	c.strokeRect(x+60,y,30,30);
-	c.strokeRect(x+90,y,30,30);
-
-}
-
-function drawSquare(c,x,y){
-	c.fillStyle ="pink";
-	c.strokeStyle = 'black';
-	c.lineWidth=1;
-	c.fillRect(x,y,30,30);
-	c.fillRect(x+30,y,30,30);
-	c.fillRect(x,y+30,30,30);
-	c.fillRect(x+30,y+30,30,30);	
-	c.strokeRect(x,y,30,30);
-	c.strokeRect(x+30,y,30,30);
-	c.strokeRect(x,y+30,30,30);
-	c.strokeRect(x+30,y+30,30,30);	
-}
-
-function drawLshape_right(c,x,y){
-	c.fillStyle ="blue";
-	c.strokeStyle = 'black';
-	c.lineWidth=1;
-	c.fillRect(x,y,30,30);
-	c.fillRect(x,y+30,30,30);
-	c.fillRect(x,y+60,30,30);
-	c.fillRect(x+30,y+60,30,30);
-	c.strokeRect(x,y,30,30);
-	c.strokeRect(x,y+30,30,30);
-	c.strokeRect(x,y+60,30,30);
-	c.strokeRect(x+30,y+60,30,30);
-}
-
-function drawTRIshape(c,x,y){
-	c.fillStyle ="purple";
-	c.strokeStyle = 'black';
-	c.lineWidth=1;
-	c.fillRect(x,y,30,30);
-	c.fillRect(x,y+30,30,30);
-	c.fillRect(x+30,y+30,30,30);
-	c.fillRect(x-30,y+30,30,30);
-	c.strokeRect(x,y,30,30);
-	c.strokeRect(x,y+30,30,30);
-	c.strokeRect(x+30,y+30,30,30);
-	c.strokeRect(x-30,y+30,30,30);
-}
-
-function drawLIshape_left(c,x,y){
-	c.fillStyle ="white";
-	c.strokeStyle = 'black';
-	c.lineWidth=1;
-	c.fillRect(x,y,30,30);
-	c.fillRect(x,y+30,30,30);
-	c.fillRect(x+30,y+30,30,30);
-	c.fillRect(x-30,y,30,30);
-	c.strokeRect(x,y,30,30);
-	c.strokeRect(x,y+30,30,30);
-	c.strokeRect(x+30,y+30,30,30);
-	c.strokeRect(x-30,y,30,30);
-}
-
-function drawLIshape_right(c,x,y){
-	c.fillStyle ="brown";
-	c.strokeStyle = 'black';
-	c.lineWidth=1;
-	c.fillRect(x,y,30,30);
-	c.fillRect(x,y+30,30,30);
-	c.fillRect(x+30,y,30,30);
-	c.fillRect(x-30,y+30,30,30);
-	c.strokeRect(x,y,30,30);
-	c.strokeRect(x,y+30,30,30);
-	c.strokeRect(x+30,y,30,30);
-	c.strokeRect(x-30,y+30,30,30);
+	window.y+=1;
 }
 
 //draw the blocks with color and board according to the maps
@@ -185,8 +96,11 @@ function drawStackBlocks(){
 		{
 			if(hasBlock[i][j]==1)
 			{
+				// Set the fill color of current paintbrush (Not the line color)
 				setBlockColor(i,j);
+				// Fill the block with color
 				c.fillRect(blockPositionMap_X[j],blockPositionMap_Y[i],30,30);
+				// Draw the boundry line of the block
 				c.strokeRect(blockPositionMap_X[j],blockPositionMap_Y[i],30,30);
 			}
 		}
